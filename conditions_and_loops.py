f76038baa8d5a28b59d7ac689cd7fedcdfe38fef
Hamna Kaleem <hamnakaleem53@gmail.com>
2025-04-05 15:59:48 +0500

    Create conditions_and_loops.py

---
+++ conditions_and_loops.py
@@ -0,0 +1,19 @@
+# If-Else
+score = 85
+
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+else:
+    print("Grade: C")
+
+# For loop
+for i in range(5):
+    print("Loop count:", i)
+
+# While loop
+count = 0
+while count < 3:
+    print("While count:", count)
+    count += 1
