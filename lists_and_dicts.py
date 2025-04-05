1b917a8b43490bfa002b2814a8fa5d9caa7c6c81
Hamna Kaleem <hamnakaleem53@gmail.com>
2025-04-05 16:00:28 +0500

    Create lists_and_dicts.py

---
+++ lists_and_dicts.py
@@ -0,0 +1,14 @@
+# Lists
+fruits = ["apple", "banana", "mango"]
+for fruit in fruits:
+    print("Fruit:", fruit)
+
+# Dictionaries
+person = {
+    "name": "John",
+    "age": 25,
+    "city": "Lahore"
+}
+
+print("Name:", person["name"])
+print("City:", person.get("city"))
